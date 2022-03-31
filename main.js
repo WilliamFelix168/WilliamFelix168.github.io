ca2aeffff86df410e213895c04369c5fc1e6fb4f
william.felix <william.felix059@gmail.com>
2022-03-31 15:52:21 +0700

    update repo github

--- main.js
+++ main.js
@@ -1,26 +1,62 @@
-// Create a request variable and assign a new XMLHttpRequest object to it.
-var request = new XMLHttpRequest()
-
-// Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://api.github.com/users/WilliamFelix168/repos', true)
-
-request.onload = function () {
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response);
-
-  console.log(data);
-
-  var statusHTML = '';
-  $.each(data, function(i, status) {
-    statusHTML += '<tr>';
-    statusHTML += '<td>' + status.id + '</td>';
-    statusHTML += '<td>' + status.name + '</td>';
-    statusHTML += '<td>' + status.html_url + '</td>';
-    statusHTML += '<td>' + status.language + '</td>';
-    statusHTML += '</tr>';
-  });
-  $('tbody').html(statusHTML);
+async function getGithubRepo() {
+  const data = await fetch(
+    "https://api.github.com/users/WilliamFelix168/repos"
+  ).then((response) => response.json());
+  updateUI(data);
 }
 
-// Send request
-request.send();
+function updateUI(card) {
+  console.log(card);
+  let cards = "";
+  card.forEach((element) => {
+    cards += showCards(element);
+  });
+
+  const container = document.querySelector(".repo-container");
+  container.innerHTML = cards;
+}
+
+function showCards(card) {
+  return `<div class="col-md-3 mb-2">
+              <div class="card bg-secondary text-white">
+                <div class="card-header text-center">
+                  ${card.name} (${card.language})
+                  <p class="language m-0 text-end mt-2">Updated : ${formatDate(card.updated_at)}</p>
+                </div>
+                <div class="card-body">
+                  <p class="card-text">${
+                    card.description ? card.description : "No description"
+                  }</p>
+                  <div class="text-center mb-2">
+                    <a href="${card.html_url}" class="btn btn-primary">Detail</a>
+                  </div>
+                </div>
+              </div>
+            </div>`;
+}
+
+
+function formatDate(date){
+  const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'June',
+    'July',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
+  ]
+
+  const d = new Date(date)
+  const year = d.getFullYear()
+  const newDate = d.getDate()
+  const monthIndex = d.getMonth()
+  const monthName = months[monthIndex]
+
+  return `${monthName} ${newDate}, ${year}`
+}
